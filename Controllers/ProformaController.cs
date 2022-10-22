58de1a67a63bcbe7d3b546f30ea872ebea9ccd3d
LeoChiarella <leochiarella2001@gmail.com>
2022-10-22 05:24:04 -0500

    ver opcion carrito

---
+++ Controllers/ProformaController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using mycellstore.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Dynamic;
+
+/// <summary>
+/// Aka Carrito
+/// </summary>
+namespace mycellstore.Controllers
+{
+
+    public class ProformaController : Controller
+    {
+        private readonly ILogger<ProformaController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager; 
+
+
+        public ProformaController(ApplicationDbContext context,
+            ILogger<ProformaController> logger,
+            UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var userIDSession = _userManager.GetUserName(User);
+            //SELECT * FROM Proforma p,Producto pr WHERE p.productId=pr.Id And p.UserId=? And p.status='PENDIENTE' 
+
+            var items = from o in _context.DataProforma select o;
+            items = items.Include(p => p.Producto).
+                    Where(w => w.UserID.Equals(userIDSession) &&
+                     w.Status.Equals("PENDIENTE"));
+            var itemsCarrito = items.ToList();
+            //Fila1 1234, Shampo; Precio, Cantidad
+            //Fila2 12345, Shampo3; Precio, Cantidad
+            var total = itemsCarrito.Sum(c => c.Cantidad * c.Precio);
+
+            //MEMORIA
+            dynamic model = new ExpandoObject();
+            model.montoTotal = total;
+            model.elementosCarrito = itemsCarrito;
+
+            //Carrito carrito = new Carrito();
+            //carrito.total = total;
+            //carrito.itemsCarrito = itemsCarrito;
+
+            //return View(carrito);
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
